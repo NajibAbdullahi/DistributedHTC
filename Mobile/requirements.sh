ee0d8f85b4046134e570a0b000524c694cb580f5
NajibAbdullahi <37065823+NajibAbdullahi@users.noreply.github.com>
2023-01-11 18:59:38 +0800

    Delete Mobile directory

--- Mobile/requirements.sh
+++
@@ -1,9 +0,0 @@
-# requirements.sh
-#!/bin/bash
-sudo python3 -m pip install seaborn
-sudo python3 -m pip install pandas
-sudo python3 -m pip install numpy
-sudo python3 -m pip install sklearn
-sudo python3 -m pip install matplotlib
-sudo python3 -m pip install category_encoders
-sudo python3 -m pip install warnings
